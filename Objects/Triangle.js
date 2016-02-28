b07b32da571d025eaa90575b5400df5e7f65b5fb
Sinho Chewi <chewisinho@berkeley.edu>
2016-02-28 00:13:29 -0800

    Finished Triangle constructor

--- Objects/Triangle.js
+++ Objects/Triangle.js
@@ -5,7 +5,7 @@ var Triangle = function(P1, P2, P3) {
 	this.angles = new Array();
 	this.name = P1.toString() + P2.toString() + P3.toString();
 	this.toString = function() {
-		return this.name;
+		return "Triangle " + this.name;
 	};
 }
 
