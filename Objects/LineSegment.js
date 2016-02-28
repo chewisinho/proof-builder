eb2547cee1fd4b77d37fdb9ccfcf57d9915ca4db
Sinho Chewi <chewisinho@berkeley.edu>
2016-02-27 17:37:55 -0800

    Added theorem descriptions

--- Objects/LineSegment.js
+++ Objects/LineSegment.js
@@ -1,11 +1,11 @@
 'use strict';
 
-/*
- * Constructor for LineSegment
- */
 var LineSegment = function(start, end) {
     this.start = start;
     this.end = end;
+    this.toString = function() {
+    	return this.start.toString() + this.end.toString();
+    };
 }
 
 define(['./Point'], function() { return LineSegment; });
--- Objects/Point.js
+++ Objects/Point.js
@@ -2,6 +2,9 @@
 
 var Point = function(name) {
     this.name = name;
+    this.toString = function() {
+    	return this.name;
+    };
 }
 
 define(function() { return Point; });
