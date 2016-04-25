71c9e8f30bbfa898c0d42288432b10c3e887b016
Christopher Pence <cpence@ucsd.edu>
2016-04-24 22:06:32 -0700

    Save.js updated, Exercise 1 proof tested and succeeded!

--- Save.js
+++ Save.js
@@ -1,14 +1,16 @@
-var Save = function(givens, goals, steps, points, lineSegments,
-                    angles, triangles, congruences, triangleCongruences) {
-    this.givens = givens;
-    this.goals = goals;
-    this.steps = steps;
-    this.points = points;
-    this.lineSegments = lineSegments;
-    this.angles = angles;
-    this.triangles = triangles;
-    this.congruences = congruences;
-    this.triangleCongruences = triangleCongruences;
+/**
+ * Save object.
+ * objects (ObjectDB) - database of all objects used in proof
+ * givens (ProofState) - state object of the start of the proof
+ * steps ([String]) - steps taken toward goal
+ * goals (ProofState) - state object of the desired end of the proof
+ */
+var Save = function(name, objects, givens, steps, goals) {
+	this.name = name;
+	this.objects = objects;
+	this.givens = givens;
+	this.steps = steps;
+	this.goals = goals;
 }
 
-define(function() { return Save; });
+module.exports = Save;
