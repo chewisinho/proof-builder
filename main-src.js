f8d8f2050c709aa941b901f23c3b8e09ecc10a4f
Christopher Pence <cpence@ucsd.edu>
2016-04-27 12:13:51 -0700

    Libraries and main js/html files.

---
+++ main-src.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var geo = require('./Logic/Objects/Geometry');
+var thm = require('./Logic/Theorems');
+var ObjectDB = require('./Logic/Objects/ObjectDB');
+var ProofState = require('./Logic/Properties/ProofState');
+//var gfx = require('./Graphics/Geometry');
+
+function main() {
+	// load theorems
+
+	// init saves
+}
+
+main();
